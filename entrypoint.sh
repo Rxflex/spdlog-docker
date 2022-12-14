0c8207229b60786178da8a6a726bcf63dd93da99
Andrey Chukhalenko <39628283+Rxflex@users.noreply.github.com>
2022-12-14 23:04:54 +0100

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,18 @@
+# Give everything time to initialize for preventing SteamCMD deadlock
+sleep 1
+
+# Default the TZ environment variable to UTC.
+TZ=${TZ:-UTC}
+export TZ
+
+# Set environment variable that holds the Internal Docker IP
+INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
+export INTERNAL_IP
+
+# Switch to the container's working directory
+cd /home/container || exit 1
+
+# Convert all of the "{{VARIABLE}}" parts of the command into the expected shell
+# variable format of "${VARIABLE}" before evaluating the string and automatically
+# replacing the values.
+PARSED=$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g' | eval echo "$(cat -)")
